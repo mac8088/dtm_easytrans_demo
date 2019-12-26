0f24b02d9cabb25cd93e909c11020cf3378eac6e
Ma Chun <chun.ma@qq.com>
2019-12-26 16:11:27 +0800

    x

--- easytrans-demo/tcc-only/tcconly-order-service/src/main/java/com/yiqiniu/easytrans/demos/order/impl/Constant.java
+++ easytrans-demo/tcc-only/tcconly-order-service/src/main/java/com/yiqiniu/easytrans/demos/order/impl/Constant.java
@@ -1,5 +1,5 @@
 package com.yiqiniu.easytrans.demos.order.impl;
 
 public class Constant {
-	public static final String APPID="order-service";
+	public static final String APPID = "order-service";
 }
